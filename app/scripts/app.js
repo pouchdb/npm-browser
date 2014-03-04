2e469ec4ebb3ee81907b673debb6097f571c0679
Nolan Lawson <nolan.lawson@gmail.com>
2014-03-04 10:25:58 -0800

    initial version, using yeoman 1.1.2

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('yeomanAngularJadeSeedApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
