074cf73c975fbf6730d8fbe9ace2fe954e2390eb
Nolan Lawson <nolan.lawson@gmail.com>
2014-04-24 14:57:52 -0700

    start syncing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,34 @@
 
 angular.module('browserNpmApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+    $scope.couchdbUrl = 'http://nolan.iriscouch.com/skimdb/';
+    $scope.docCount = 0;
+    $scope.remoteDocCount = 0;
+
+    var pouch = new PouchDB('npm');
+    var remotePouch = new PouchDB($scope.couchdbUrl);
+
+    function fetchDocCount() {
+      pouch.info().then(function (res) {
+        $scope.docCount = res.doc_count;
+        $scope.$apply();
+      });
+    }
+
+    function fetchRemoteDocCount() {
+      remotePouch.info().then(function (res) {
+        $scope.remoteDocCount = res.doc_count;
+        $scope.$apply();
+      });
+    }
+
+    fetchRemoteDocCount();
+
+    pouch.replicate.from(remotePouch, {
+      onChange: fetchDocCount,
+      complete: function () {
+        $scope.syncComplete = true;
+      }
+    });
   });
