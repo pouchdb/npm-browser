c8eea1f2ce889e70fd6e9d4926bb132bf788ad76
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-07 16:01:23 -0700

    add pagination

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,16 @@
 angular.module('browserNpmApp')
   .controller('MainCtrl', function ($scope) {
 
+    var PAGE_SIZE = 10;
+
     $scope.couchdbUrl = 'http://nolan.iriscouch.com/skimdb/';
     $scope.docCount = 0;
     $scope.remoteDocCount = 0;
+    $scope.page = [];
+    $scope.pageStack = [];
 
+    var dirty = false;
+    var startkey;
     var pouch = new PouchDB('npm');
     var remotePouch = new PouchDB($scope.couchdbUrl);
 
@@ -24,12 +30,65 @@ angular.module('browserNpmApp')
       });
     }
 
-    fetchRemoteDocCount();
+    function updatePage() {
+      fetchDocCount();
 
+      if ($scope.loading) {
+        dirty = true;
+        return;
+      }
+
+      $scope.loading = true;
+      var opts = {
+        limit: PAGE_SIZE,
+        include_docs: true
+      };
+      if (startkey) {
+        opts.startkey = startkey;
+        opts.skip = 1;
+      }
+
+      pouch.allDocs(opts).then(function (res) {
+        $scope.page = res.rows.map(function (row) {
+          row.doc.maintainersString = (row.doc.maintainers || []).map(function (person) {
+            return person.name
+          }).join(', ');
+          return row.doc;
+        });
+        $scope.loading = false;
+        if (dirty) {
+          dirty = false;
+          updatePage();
+        }
+        $scope.$apply();
+      });
+    }
+
+    var counter = 0;
     pouch.replicate.from(remotePouch, {
-      onChange: fetchDocCount,
+      onChange: function() {
+        if (++counter % 10 === 0) {
+          updatePage();
+        }
+      },
       complete: function () {
         $scope.syncComplete = true;
       }
     });
+    fetchRemoteDocCount();
+    updatePage();
+
+    $scope.nextPage = function() {
+      var lastDoc = $scope.page[$scope.page.length - 1];
+      if (lastDoc) {
+        $scope.pageStack.push(startkey);
+        startkey = lastDoc._id;
+        updatePage();
+      }
+    }
+
+    $scope.previousPage = function() {
+      startkey = $scope.pageStack.pop();
+      updatePage();
+    }
   });
